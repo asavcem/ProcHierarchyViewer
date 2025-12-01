02e1b5948953d3dcb49a45d8cc90fb186f93f7d5
asavcem <asavcem@gmail.com>
2025-12-01 14:18:08 +0300

    ConnectionString, appconfig'den okunacak şekilde değiştirildi.

--- ProcHierarchyViewer/Program.cs
+++ ProcHierarchyViewer/Program.cs
@@ -7,25 +7,26 @@ using ProcHierarchyViewer.Views;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace ProcHierarchyViewer
 {
     static class Program
     {
-        private const string ConnectionString = "(value)";  
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            string connStr = ConfigurationManager.ConnectionStrings["MainDb"].ConnectionString;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Manuel bağımlılık oluşturma
 
-            var repository = new SqlProcRepository(ConnectionString);
+            var repository = new SqlProcRepository(connStr);
             var service = new ProcHierarchyService(repository);
             var presenter = new MainPresenter(service);
             var mainForm = new MainForm(presenter);
